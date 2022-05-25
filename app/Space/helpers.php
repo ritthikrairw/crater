52ad7d42e86e98593ac233b53f75a8ac281e6359
Ritthikrai W <ritthikrai.rw@gmail.com>
2022-05-25 21:54:17 +0700

    add new helper function

--- app/Space/helpers.php
+++ app/Space/helpers.php
@@ -129,7 +129,7 @@ function format_money_pdf($money, $currency = null)
 {
     $money = $money / 100;
 
-    if (! $currency) {
+    if (!$currency) {
         $currency = Currency::findOrFail(CompanySetting::getSetting('currency', 1));
     }
 
@@ -142,12 +142,85 @@ function format_money_pdf($money, $currency = null)
 
     $currency_with_symbol = '';
     if ($currency->swap_currency_symbol) {
-        $currency_with_symbol = $format_money.'<span style="font-family: DejaVu Sans;">'.$currency->symbol.'</span>';
+        $currency_with_symbol = $format_money . '<span style="font-family: DejaVu Sans;">' . $currency->symbol . '</span>';
     } else {
-        $currency_with_symbol = '<span style="font-family: DejaVu Sans;">'.$currency->symbol.'</span>'.$format_money;
+        $currency_with_symbol = '<span style="font-family: DejaVu Sans;">' . $currency->symbol . '</span>' . $format_money;
     }
 
     return $currency_with_symbol;
+}
+
+
+/**
+ * It takes a number, converts it to a string, splits it into two parts, converts the first part to
+ * Thai, converts the second part to Thai, and then concatenates the two parts together.
+ *
+ * @param price The price you want to convert to Thai string.
+ *
+ * @return the string representation of the number in Thai.
+ */
+function convert_price_to_thai_string_pdf($price)
+{
+
+    $price = $price / 100;
+
+    $amount_number = number_format($price, 2, '.', '');
+    $pt = strpos($amount_number, '.');
+    $number = $fraction = '';
+    if ($pt === false) {
+        $number = $amount_number;
+    } else {
+        $number = substr($amount_number, 0, $pt);
+        $fraction = substr($amount_number, $pt + 1);
+    }
+
+    $ret = '';
+    $baht = read_number_thai($number);
+    if ($baht != '') {
+        $ret .= $baht . 'บาท';
+    }
+
+    $satang = read_number_thai($fraction);
+    if ($satang != '') {
+        $ret .= $satang . 'สตางค์';
+    } else {
+        $ret .= 'ถ้วน';
+    }
+    return $ret;
+}
+
+/**
+ * It takes a number and returns a string with the Thai equivalent
+ *
+ * @param number The number to be converted.
+ *
+ * @return the number in Thai.
+ */
+function read_number_thai($number)
+{
+    $position_call = ['แสน', 'หมื่น', 'พัน', 'ร้อย', 'สิบ', ''];
+    $number_call = ['', 'หนึ่ง', 'สอง', 'สาม', 'สี่', 'ห้า', 'หก', 'เจ็ด', 'แปด', 'เก้า'];
+    $number = $number + 0;
+    $ret = '';
+    if ($number == 0) {
+        return $ret;
+    }
+    if ($number > 1000000) {
+        $ret .= readNumber(intval($number / 1000000)) . 'ล้าน';
+        $number = intval(fmod($number, 1000000));
+    }
+
+    $divider = 100000;
+    $pos = 0;
+    while ($number > 0) {
+        $d = intval($number / $divider);
+        $ret .= $divider == 10 && $d == 2 ? 'ยี่' : ($divider == 10 && $d == 1 ? '' : ($divider == 1 && $d == 1 && $ret != '' ? 'เอ็ด' : $number_call[$d]));
+        $ret .= $d ? $position_call[$pos] : '';
+        $number = $number % $divider;
+        $divider = $divider / 10;
+        $pos++;
+    }
+    return $ret;
 }
 
 /**
@@ -157,21 +230,21 @@ function format_money_pdf($money, $currency = null)
 function clean_slug($model, $title, $id = 0)
 {
     // Normalize the title
-    $slug = Str::upper('CUSTOM_'.$model.'_'.Str::slug($title, '_'));
+    $slug = Str::upper('CUSTOM_' . $model . '_' . Str::slug($title, '_'));
 
     // Get any that could possibly be related.
     // This cuts the queries down by doing it once.
     $allSlugs = getRelatedSlugs($model, $slug, $id);
 
     // If we haven't used it before then we are all good.
-    if (! $allSlugs->contains('slug', $slug)) {
+    if (!$allSlugs->contains('slug', $slug)) {
         return $slug;
     }
 
     // Just append numbers like a savage until we find not used.
     for ($i = 1; $i <= 10; $i++) {
-        $newSlug = $slug.'_'.$i;
-        if (! $allSlugs->contains('slug', $newSlug)) {
+        $newSlug = $slug . '_' . $i;
+        if (!$allSlugs->contains('slug', $newSlug)) {
             return $newSlug;
         }
     }
@@ -181,7 +254,7 @@ function clean_slug($model, $title, $id = 0)
 
 function getRelatedSlugs($type, $slug, $id = 0)
 {
-    return CustomField::select('slug')->where('slug', 'like', $slug.'%')
+    return CustomField::select('slug')->where('slug', 'like', $slug . '%')
         ->where('model_type', $type)
         ->where('id', '<>', $id)
         ->get();
